06bc84e239cea591200fbe94a6ab424237c387c7
Ivan Allen Macabontoc <ivan_dev@my.pisopay.com.ph>
2025-11-19 15:17:13 +0800

    change index content

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
-import Jwt from './jwt/token.js';
-import TokenSegment from './jwt/segment.js';
-import TokenValidate from './jwt/validator.js';
-
-export default { Jwt, TokenSegment, TokenValidate };
+export { default as Jwt } from './jwt/token.js';
+export { default as Validator } from './jwt/validator.js';
